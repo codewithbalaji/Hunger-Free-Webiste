ddf3e5af3f9087672f2c5baaa51502e11af9dd51
codewithbalaji <balaji01975@gmail.com>
2024-04-16 20:53:02 +0530

    fixed scss

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import "../src/css/main.css";
+import './main.scss';
 import "bootstrap-icons/font/bootstrap-icons.css"; 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
